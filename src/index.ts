1c6bc61fe017a72f827d72fe37c59a349141367c
Chau Dinh <chaudvb95uc@gmail.com>
2020-05-03 21:43:02 +0700

    creating user with typeorm

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,16 @@
 import "reflect-metadata";
-
+import { importSchema } from "graphql-import";
 import { GraphQLServer } from "graphql-yoga";
+import { createConnection } from "typeorm";
+import * as path from "path";
 
-const typeDefs = `
-  type Query {
-    hello(name: String): String!
-  }
-`;
+import { resolvers } from "./resolvers";
 
-const resolvers = {
-  Query: {
-    hello: (_: any, { name }: any) => `Bye ${name || "World"}`,
-  },
-};
+const typeDefs = importSchema(path.join(__dirname, "./schema.graphql"));
 
 const server = new GraphQLServer({ typeDefs, resolvers });
-server.start(() => console.log("Server is running on localhost:4000"));
+
+// Connect to the database
+createConnection().then(() => {
+  server.start(() => console.log("🎉 Server is running on localhost:4000"));
+});
