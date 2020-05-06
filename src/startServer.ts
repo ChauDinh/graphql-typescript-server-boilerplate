214b1be55157246d430965f6c470d983e1d8b4b2
Chau Dinh <chaudvb95uc@gmail.com>
2020-05-06 19:53:43 +0700

    generate schema with @gql2ts/from-schema package

--- src/startServer.ts
+++ src/startServer.ts
@@ -1,28 +1,13 @@
-import { importSchema } from "graphql-import";
+import { generateSchema } from "./utils/generateSchema";
 import { GraphQLServer } from "graphql-yoga";
-import * as path from "path";
-import * as fs from "fs";
-import { mergeSchemas, makeExecutableSchema } from "graphql-tools";
-import { GraphQLSchema } from "graphql";
 
 import { redis } from "./redis";
 import { createTypeORMConnection } from "./utils/createTypeORMConnection";
 import confirmRoute from "./routes/confirmEmail.route";
 
 export const startServer = async () => {
-  const schemas: GraphQLSchema[] = [];
-  const folders = fs.readdirSync(path.join(__dirname, "./modules"));
-  folders.forEach((folder) => {
-    const { resolvers } = require(`./modules/${folder}/${folder}.resolver`);
-    const typeDefs = importSchema(
-      path.join(__dirname, `./modules/${folder}/${folder}.schema.graphql`)
-    );
-
-    schemas.push(makeExecutableSchema({ resolvers, typeDefs }));
-  });
-
   const server = new GraphQLServer({
-    schema: mergeSchemas({ schemas }),
+    schema: generateSchema(),
     context: ({ request }) => ({
       redis,
       url: request.protocol + "://" + request.get("host"),
---
+++ src/utils/generateSchema.ts
@@ -0,0 +1,20 @@
+import * as path from "path";
+import * as fs from "fs";
+import { mergeSchemas, makeExecutableSchema } from "graphql-tools";
+import { GraphQLSchema } from "graphql";
+import { importSchema } from "graphql-import";
+
+export const generateSchema = () => {
+  const schemas: GraphQLSchema[] = [];
+  const folders = fs.readdirSync(path.join(__dirname, "../modules"));
+  folders.forEach((folder) => {
+    const { resolvers } = require(`../modules/${folder}/${folder}.resolver`);
+    const typeDefs = importSchema(
+      path.join(__dirname, `../modules/${folder}/${folder}.schema.graphql`)
+    );
+
+    schemas.push(makeExecutableSchema({ resolvers, typeDefs }));
+  });
+
+  return mergeSchemas({ schemas });
+};
