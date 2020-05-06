e3028ea40b5339c8c38c58f5434e24bebf398529
Chau Dinh <chaudvb95uc@gmail.com>
2020-05-06 17:53:25 +0700

    fix connection error: start the typeorm server only one

--- src/jestGlobalSetup/callSetup.js
+++ src/jestGlobalSetup/callSetup.js
@@ -1,8 +1,10 @@
 // reference setup module
 const { setup } = require("./setup");
 
-module.exports = async function () {
-  // Call the initialization methods
-  await setup();
-  return null;
+module.exports = async function() {
+	// Call the initialization methods
+	if (!process.env.TEST_HOST) {
+		await setup();
+	}
+	return null;
 };
